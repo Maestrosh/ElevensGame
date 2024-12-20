2e988b99d244a8e73708fd00e6c419878a0c55e3
Mehdi <mehdi.shakibapour@gmail.com>
2024-12-20 15:45:46 -0500

    Update Deck.cs

--- Deck.cs
+++ Deck.cs
@@ -1,16 +1,34 @@
-//Deck.cs
-
+/// <summary>
+/// Represents a deck of playing cards with functionalities to shuffle, deal, and manage the cards.
+/// </summary>
 using System;
 using System.Collections.Generic;
 
-// Deck Class: Manages a deck of playing cards
 public class Deck
 {
+    /// <summary>
+    /// The list of cards in the deck.
+    /// </summary>
     private List<Card> cards;
+
+    /// <summary>
+    /// The suits available in a standard deck of cards.
+    /// </summary>
     private static readonly string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+
+    /// <summary>
+    /// The ranks available in a standard deck of cards.
+    /// </summary>
     private static readonly string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+    /// <summary>
+    /// The corresponding values for each rank.
+    /// </summary>
     private static readonly int[] values = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1 };
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Deck"/> class and populates it with a standard set of 52 playing cards.
+    /// </summary>
     public Deck()
     {
         cards = new List<Card>();
@@ -23,6 +41,9 @@ public class Deck
         }
     }
 
+    /// <summary>
+    /// Shuffles the deck using the Fisher-Yates algorithm.
+    /// </summary>
     public void Shuffle()
     {
         Random rng = new Random();
@@ -33,6 +54,11 @@ public class Deck
         }
     }
 
+    /// <summary>
+    /// Deals the top card from the deck.
+    /// </summary>
+    /// <returns>The top <see cref="Card"/> from the deck.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when the deck is empty.</exception>
     public Card Deal()
     {
         if (cards.Count == 0) throw new InvalidOperationException("The deck is empty.");
@@ -41,6 +67,10 @@ public class Deck
         return dealtCard;
     }
 
+    /// <summary>
+    /// Gets the number of cards remaining in the deck.
+    /// </summary>
+    /// <returns>The number of cards remaining.</returns>
     public int CardsRemaining()
     {
         return cards.Count;
